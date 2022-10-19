87f7e1d096f9014b2d576638ae60813b90dade9e
hustdog <58131815+hustdog@users.noreply.github.com>
2022-10-19 16:33:52 +0800

    Delete sort_20.py

--- sort_20.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-df = pd.read_csv('个股价格.csv')
-
-avg = df.groupby(df['股票代码']).mean()
-weight = avg.eval('权重求和 = 2*日个股交易股数+2*日个股交易金额+2*日个股流通市值+2*日个股总市值 +2*日个股回报率')
-weight_sort = weight.sort_values(by='权重求和',ascending=False)
-
-weight_sort_20 = weight_sort[0:19]
-weight_sort_20.to_excel('sort_20.xlsx')
